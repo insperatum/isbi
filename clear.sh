00119f4189342693206855d02f1a492b02b9cb79
root <root@ip-172-31-0-105.eu-west-1.compute.internal>
2015-06-03 00:25:44 +0000

    NEUROPROOF

--- clear.sh
+++ clear.sh
@@ -1,6 +1,6 @@
 echo CLEARING...
-rm -rf /mnt/isbi_models; rm -rf /mnt/isbi_predictions;
+rm -rf /mnt/neuroproof_models; rm -rf /mnt/neuroproof_predictions;
 (cat ~/spark-ec2/slaves) | (while read line; do
- ssh -n -o StrictHostKeyChecking=no -t -t root@$line "rm -rf /mnt/isbi_predictions" & 
+ ssh -n -o StrictHostKeyChecking=no -t -t root@$line "rm -rf /mnt/neuroproof_predictions" & 
   tasks="$tasks $!"; done; for t in $tasks; do wait $t; done);
   mkdir /root/logs
